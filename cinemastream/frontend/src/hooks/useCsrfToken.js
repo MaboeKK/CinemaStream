5fe32848b78c69e479c3f5b41df5b22baa0ae03f
Keiketlile-Maboe <KMaboe@datacentrix.co.za>
2025-06-19 10:24:14 +0200

    fix

--- cinemastream/frontend/src/hooks/useCsrfToken.js
+++ cinemastream/frontend/src/hooks/useCsrfToken.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getCsrfToken = async () => {
   try {
-    const response = await axios.get('45.220.164.64:5000/api/auth/csrf-token', { withCredentials: true });
+    const response = await axios.get('/api/auth/csrf-token', { withCredentials: true });
     return response.data.csrfToken;
   } catch (error) {
     console.error('Error fetching CSRF Token:', error);
