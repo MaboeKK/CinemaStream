76db8a166966d1277c9dc9ec5405d0777ef3dbee
Magonna Mdebuka <MMdebuka@datacentrix.co.za>
2025-07-10 18:18:27 +0200

    Cleaned the row and removed unused series state

--- cinemastream/frontend/src/Components/Rows/Row.js
+++ cinemastream/frontend/src/Components/Rows/Row.js
@@ -4,7 +4,6 @@ import MovieCard from '../MovieCard/MovieCard';
 
 function Row({ title, fetchFunction, onMovieClick }) {
   const [movies, setMovies] = useState([]);
-  const [series, setSeries] = useState([]);
   const rowRef = useRef(null);
 
   useEffect(() => {
@@ -14,17 +13,6 @@ function Row({ title, fetchFunction, onMovieClick }) {
     };
     loadMovies();
   }, [fetchFunction]);
-
-//   useEffect(() => {
-//   const loadSeries = async () => {
-//     const series = await fetchFunction();
-//     if (!series.length) {
-//       console.warn("No series fetched");
-//     }
-//     setSeries(series);
-//   };
-//   loadSeries();
-// }, []);
 
   const scrollLeft = () => {
     rowRef.current.scrollBy({ left: -500, behavior: 'smooth' });
@@ -43,9 +31,6 @@ function Row({ title, fetchFunction, onMovieClick }) {
           {movies.map((movie, index) => (
             <MovieCard key={index} movie={movie} onClick={() => onMovieClick(movie)} />
           ))}
-           {/* {series.map((series, index) => (
-            <MovieCard key={index} series={series} onClick={() => onMovieClick(series)} />
-          ))} */}
         </div>
         <button className="scroll-button right" onClick={scrollRight}>&gt;</button>
       </div>
@@ -54,4 +39,3 @@ function Row({ title, fetchFunction, onMovieClick }) {
 }
 
 export default Row;
-
